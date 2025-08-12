700515079244239287b28d79558e5917fdbde660
Caglar-Demir <caglard3599@gmail.com>
2025-08-12 16:04:19 +0300

    First Project: Color Flipper completed.

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025', 'cyan'];
+
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    const random_number = getRandomNumber()
+    
+    document.body.style.backgroundColor = colors[random_number];
+    color.textContent = colors[random_number];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
